7afff0f94a05617bd8c3fe9d35b63bc8300f16c4
Mostafa Osman <mostafa233.osman@gmail.com>
2025-05-27 14:04:01 +0300

    add method fetchFiveDayForecast

--- lib/repositories/weather_repository.dart
+++ lib/repositories/weather_repository.dart
@@ -3,7 +3,7 @@ import '../models/weather_model.dart';
 
 class WeatherRepository {
   final Dio _dio = Dio();
-  final String _apiKey = 'YOUR_API_KEY'; // استبدله بمفتاحك
+  final String _apiKey = '515efa1006d0ed6fec3cbc6769e92b27'; 
 
   Future<Weather> fetchWeather(String city) async {
     final response = await _dio.get(
@@ -21,4 +21,23 @@ class WeatherRepository {
       throw Exception('فشل في جلب البيانات');
     }
   }
+  Future<List<Weather>> fetchFiveDayForecast(String city) async {
+  final response = await _dio.get(
+    'https://api.openweathermap.org/data/2.5/forecast',
+    queryParameters: {
+      'q': city,
+      'appid': _apiKey,
+      'units': 'metric',
+    },
+  );
+
+  if (response.statusCode == 200) {
+    final List list = response.data['list'];
+    return list.where((e) => list.indexOf(e) % 8 == 0).map((json) {
+      return Weather.fromJson(json..['name'] = city);
+    }).toList();
+  } else {
+    throw Exception('فشل في جلب التوقعات');
+  }
+}
 }
